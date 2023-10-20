24bbbd0e9347e63f41e502fc53a803e79bdec8e4
TheBaytur <bayturcodding@gmail.com>
2023-10-20 20:08:18 +0800

    Triangle Throws Exception

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,9 @@ public class Main {
     public Main(int sideA, int sideB, int sideC) {
 
     }
-
     public static void main(String[] args) throws IllegalAccessException {
         Triangle triangle = new Triangle(1,2,3);
     }
 
-
+// Triangle throws Exception: Wrong arguments to create triangle with sides 1 2 3
 }
