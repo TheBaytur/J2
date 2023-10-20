6785d657dba1284ee558dc7cf9a5f94805c6aa51
TheBaytur <bayturcodding@gmail.com>
2023-10-20 20:06:38 +0800

    Throw Exception

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import Triangle.Triangle;
+
+public class Main {
+
+    public Main(int sideA, int sideB, int sideC) {
+
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Triangle triangle = new Triangle(1,2,3);
+    }
+
+
+}
